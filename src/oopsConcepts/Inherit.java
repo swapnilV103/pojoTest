e34a44cd7a66ed3644f9ec8da76d1d3944b30865
Swapnil Idawate <swapnilidawate103@gmail.com>
2024-08-09 11:41:12 +0530

    inheritance

---
+++ src/oopsConcepts/Inherit.java
@@ -0,0 +1,19 @@
+package oopsConcepts;
+
+
+class bike{
+	public String name= " Honda";
+	
+}
+
+public class Inherit extends bike{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		bike car = new bike();
+		
+		System.out.println(car.name);
+
+	}
+
+}
